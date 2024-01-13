c711e1b1465d48894d04740c2bb79c01dc8a91b9
alithux <156514240+alithux@users.noreply.github.com>
2024-01-13 19:08:53 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-//objects
+let todos = document.querySelectorAll('li.todo-item');
 
-let user = {
-    firstName: 'Mohamed' ,
-    
+todos.forEach(item => {
 
-}
+    item.addEventListener('click', event => {
+       let clickedItem = event.target;
+
+       
+       if (clickedItem.tagName !== 'LI') {
+            clickedItem = event.target.parentElement;
+
+       }
+
+       if (event.target.tagName === 'A') {
+        clickedItem.remove();
+
+   }
+
+       let checkbox = clickedItem.querySelector('input');
+       let label = clickedItem.querySelector('label');
+
+
+       if(checkbox.checked) {
+            checkbox.checked = false;
+            label.style.textDecoration = '';
+       } else {
+            checkbox.checked= true;
+            label.style.textDecoration = 'line-through'
+       }
+    })
+});
